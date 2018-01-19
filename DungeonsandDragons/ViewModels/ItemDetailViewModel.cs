04a46b45938fd7571b135445a488cfbaafb39b20
Leo <linh3@seattleu.edu>
2018-01-18 16:10:38 -0800

    Create Project

---
+++ DungeonsandDragons/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DungeonsandDragons
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}

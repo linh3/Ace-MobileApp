068de3c1791e8d35be68997c04b2e84aa5fbbae9
Leo <linh3@seattleu.edu>
2018-01-21 15:03:47 -0800

    Add Global class, hero class, and Character class

---
+++ DungeonsandDragons/Models/Character.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+namespace DungeonsandDragons
+{     
+    public class Character
+    {
+        protected string Name;
+        protected string ImageLink;
+        protected int Strength;
+       // protected int TotalStrength;
+        protected int Speed;
+        protected int MaxHealth;
+        protected int Health;
+        protected int Level;
+        protected int Experience;
+        protected int Defense;
+        protected int [] Items; // Instead of having a list of int, we will have an list of item
+
+        public Character()
+        {
+            Name = "";
+            ImageLink = "";
+            Strength = 0;
+            //TotalStrength = 0;
+            Speed = 0;
+            MaxHealth = 0;
+            Health = 0;
+            Level = 0;
+            Experience = 0;
+            Defense = 0;
+            Items = null;
+        }
+
+        public void setName(string name) => this.Name = name;
+
+        public string getName() => this.Name;
+
+        public void setImageLink(string link) => this.ImageLink = link;
+
+        public string getImageLink() => this.ImageLink;
+
+        public void setStrength(int strength) => this.Strength = strength;
+
+        public int getStrength() => this.Strength;
+
+        public void setSpeed(int speed) => this.Speed = speed;
+
+        public int getSpeed() => this.Speed;
+
+        public void setMaxHealth(int maxhealth) => this.MaxHealth = maxhealth;
+
+        public int getMaxHealth() => this.MaxHealth;
+
+        public void setHealth(int health) => this.Health = health;
+
+        public int getHealth() => this.Health;
+
+        public void setLevel(int level) => this.Level = level;
+
+        public int getLevel() => this.Level;
+
+        public void setExperience(int experience) => this.Experience = experience;
+
+        public int getExperience() => this.Experience;
+
+        public void setDefense(int defense) => this.Defense = defense;
+
+        public int getDefense() => this.Defense;
+
+        public int Attack() => this.Strength;
+
+        public void Takedamage(int damage)
+        {
+            this.Health -= damage;
+        }
+
+        public bool checkDead()
+        {
+            if(this.Health <= 0){
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+    }
+}
---
+++ DungeonsandDragons/Models/Hero.cs
@@ -0,0 +1,165 @@
+﻿using System;
+namespace DungeonsandDragons
+{
+    public class Hero:Character
+    {
+        public Hero()
+        {
+            Name = "";
+            ImageLink = "";
+            Strength = 1;
+            //TotalStrength = 1;
+            Speed = 1;
+            MaxHealth = 10;
+            Health = 10;
+            Level = 1;
+            Experience = 0;
+            Defense = 1;
+            Items = null;
+            this.Items = new int[Global.MAXITEM];
+        }
+
+        public void gainExperience(int experience)
+        {
+            this.Experience += experience;
+        }
+
+        public bool LevelUp()
+        {
+            if (this.Level == 1 && this.Experience >= 300)
+            {
+                this.Level++;
+                this.Defense++;
+                return true;
+            }
+            else if (this.Level == 2 && this.Experience >= 900)
+            {
+                this.Level++;
+                this.Strength++;
+                this.Defense++;
+                return true;
+
+            }
+            else if (this.Level == 3 && this.Experience >= 2700)
+            {
+                this.Level++;
+                return true;
+            }
+            else if (this.Level == 4 && this.Experience >= 6500)
+            {
+                this.Level++;
+                this.Defense++;
+                this.Speed++;
+                return true;
+            }
+            else if (this.Level == 5 && this.Experience >= 14000)
+            {
+                this.Level++;
+                this.Strength++;
+                return true;
+            }
+            else if (this.Level == 6 && this.Experience >= 23000)
+            {
+                this.Level++;
+                this.Defense++;
+                return true;
+            }
+            else if (this.Level == 7 && this.Experience >= 34000)
+            {
+                this.Level++;
+                return true;
+            }
+            else if (this.Level == 8 && this.Experience >= 48000)
+            {
+                this.Level++;
+                return true;
+            }
+            else if (this.Level == 9 && this.Experience >= 64000)
+            {
+                this.Level++;
+                this.Strength++;
+                this.Defense++;
+                this.Speed++;
+                return true;
+            }
+            else if (this.Level == 10 && this.Experience >= 85000)
+            {
+                this.Level++;
+                return true;
+            }
+            else if (this.Level == 11 && this.Experience >= 100000)
+            {
+                this.Level++;
+                return true;
+            }
+            else if (this.Level == 12 && this.Experience >= 120000)
+            {
+                this.Level++;
+                this.Defense++;
+                return true;
+            }
+            else if (this.Level == 13 && this.Experience >= 140000)
+            {
+                this.Level++;
+                this.Strength++;
+                return true;
+            }
+            else if (this.Level == 14 && this.Experience >= 165000)
+            {
+                this.Level++;
+                this.Speed++;
+                return true;
+            }
+            else if (this.Level == 15 && this.Experience >= 195000)
+            {
+                this.Level++;
+                this.Defense++;
+                return true;
+            }
+            else if (this.Level == 16 && this.Experience >= 225000)
+            {
+                this.Level++;
+                return true;
+            }
+            else if (this.Level == 17 && this.Experience >= 265000)
+            {
+                this.Level++;
+                this.Strength++;
+                return true;
+            }
+            else if (this.Level == 18 && this.Experience >= 305000)
+            {
+                this.Level++;
+                this.Strength++;
+                this.Defense++;
+                return true;
+            }
+            else if (this.Level == 19 && this.Experience >= 355000)
+            {
+                this.Level++;
+                this.Strength++;
+                this.Defense++;
+                this.Speed++;
+                return true;
+            }
+            else{
+                return false;   
+            }
+        }
+        public bool useItem(int item, int location)
+        {
+            if(location < 0 || location >= Global.MAXITEM || this.Items[location]!=0 /* location has been used*/){
+                return false;
+            }else{
+                Items[location] = item;
+                //also need to update the hero attributes (strength, defense...) before return
+                return true;
+            }
+        }
+
+        public int dropItem(int location){
+            return this.Items[location];
+        }
+
+    }
+}

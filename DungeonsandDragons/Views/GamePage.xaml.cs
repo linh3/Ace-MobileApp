55e03db9d137a6e6f0210351faf09943b9ef74a5
Leo <linh3@seattleu.edu>
2018-03-19 18:02:36 -0700

    Correct need two buttonclick for AutoBattle

--- DungeonsandDragons/Views/GamePage.xaml.cs
+++ DungeonsandDragons/Views/GamePage.xaml.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using DungeonsandDragons.Models;
+using DungeonsandDragons.ViewModels;
+
 
 namespace DungeonsandDragons
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class GamePage : ContentPage
     {
+        AutoBattleViewModel viewModel;
+
         public GamePage()
         {
+            BindingContext = viewModel = new AutoBattleViewModel();
+            viewModel.LoadDataCommand.Execute(null);
             InitializeComponent();
         }
         async public void StartBattle_Clicked(object sender, EventArgs e)
@@ -16,9 +26,66 @@ namespace DungeonsandDragons
             await Navigation.PushAsync(new BattlePage());
         }
 
-        async public void AutoBattle_Clicked(object sender, EventArgs e)
+        protected override void OnAppearing()
         {
-            await Navigation.PushAsync(new AutoBattlePage());
+
+
+            base.OnAppearing();
+            BindingContext = null;
+            if (ToolbarItems.Count > 0)
+            {
+                ToolbarItems.RemoveAt(0);
+            }
+
+            InitializeComponent();
+            viewModel.LoadDataCommand.Execute(null);
+            if (viewModel.NeedsRefresh())
+            {
+                viewModel.LoadDataCommand.Execute((null));
+            }
+
+            BindingContext = viewModel;
+
+        }
+     
+
+        async public void GameOver()
+        {
+            Score score = new Score();
+            score.ScoreTotal = viewModel.Battle.round;
+            MessagingCenter.Send(this, "AddData", score);
+            await Navigation.PushAsync(new ScoreDetailPage(new ScoreDetailViewModel(score)));
+
+        }
+
+        public void TakeTurn()
+        {
+            viewModel.TakeTurn();
+        }
+
+        public void NewRound()
+        {
+
+            viewModel.NextRound();
+        }
+        public void AutoBattleButtonClicked(object sender, EventArgs e)
+        {
+            viewModel.Battle.round = 0;
+            this.NewRound();
+            while (!viewModel.isGameOver())
+            {
+                if (viewModel.Battle.isAllMonstersDead() && !viewModel.Battle.isAllHeoresDead())
+                {
+                    this.NewRound();
+                }
+
+                this.TakeTurn();
+            }
+
+            InitializeComponent();
+            BindingContext = viewModel;
+            this.GameOver();
+
         }
     }
 }
